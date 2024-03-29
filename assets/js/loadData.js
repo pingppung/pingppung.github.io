dd51eb30766a1e486623ac8b0406f1f904fe60e3
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-30 02:25:40 +0900

    포트폴리오 프로젝트 배치 ver. 1.0.1

--- assets/js/loadData.js
+++ assets/js/loadData.js
@@ -1,34 +1,61 @@
 import projectsData from "./projectsData.js";
 import skillData from "./skillData.js";
 document.addEventListener("DOMContentLoaded", function () {
+  skillData.loadData(); //스킬 불러오기
+  projectsData.loadData();
+
   const navItems = document.querySelectorAll("header nav ul li");
+
+  //상단 바를 이용해서 이동하는 경우
   navItems.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
       const sectionText = this.textContent.toLowerCase().trim();
+      scrollToSection(sectionText);
+    });
+  });
 
-      const section = document.querySelector("section#" + sectionText);
-      if (section) {
-        section.scrollIntoView({ behavior: "smooth" });
+  //스크롤로 해당 세션 지나가는 경우
+  const sections = document.querySelectorAll("section");
+  const sectionOffsets = [];
+  sections.forEach((section) => {
+    sectionOffsets.push(section.offsetTop);
+  });
+  window.addEventListener("scroll", handleScroll);
+
+  function handleScroll() {
+    const scrollPosition = window.scrollY + 300;
+    let activeSectionIndex = -1;
+    for (let i = 0; i < sectionOffsets.length; i++) {
+      if (scrollPosition >= sectionOffsets[i]) {
+        activeSectionIndex = i;
+      } else {
+        break; // 스크롤 위치가 섹션의 위쪽에 있을 때 반복문 종료
       }
-
-      navItems.forEach(function (navItem) {
-        navItem.classList.remove("select");
-      });
-
-      this.classList.add("select");
+    }
+    if (activeSectionIndex === -1) {
+      activeSectionIndex = 0; // 스크롤 위치가 첫 번째 섹션의 위에 있을 때
+    }
+    navItems.forEach((link) => {
+      link.classList.remove("select");
     });
-  });
+    navItems[activeSectionIndex].classList.add("select");
+  }
 
-  const projects = document.querySelectorAll(".project-list ul li");
-  projects.forEach((project) => {
-    project.addEventListener("click", function () {
-      const projectName = this.getAttribute("data-project");
-      projectsData.loadData(projectName);
+  function scrollToSection(sectionText) {
+    const section = document.querySelector("section#" + sectionText);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+    // 버튼에 active 클래스 추가
+    navItems.forEach((navItem) => {
+      navItem.classList.remove("select");
     });
-  });
-
-  skillData.loadData(); //스킬 불러오기
-  const defaultProject = projects[0].getAttribute("data-project"); // 기본 프로젝트 설정
-  projectsData.loadData(defaultProject);
+    const clickedNavItem = Array.from(navItems).find((navItem) => {
+      return navItem.textContent.trim().toLowerCase() === sectionText;
+    });
+    if (clickedNavItem) {
+      clickedNavItem.classList.add("select");
+    }
+  }
 });
--- assets/js/projectsData.js
+++ assets/js/projectsData.js
@@ -1,10 +1,10 @@
 const projectsData = {
-  loadData: function (projectName) {
+  loadData: function () {
     fetch("/data/projects.json")
       .then((response) => response.json())
       .then((data) => {
-        const project = data.find((item) => item.subject == projectName);
-        this.createProject(project);
+        //const project = data.find((item) => item.subject == projectName);
+        this.createProject(data);
       })
       .catch((error) =>
         console.error(
@@ -14,35 +14,65 @@ const projectsData = {
       );
   },
 
-  createProject: function (project) {
-    const projectSection = document.querySelector("#projects .pj");
+  createProject: function (data) {
+    const projectSection = document.querySelector(".projects-list");
+    data.forEach((project) => {
+      const projectDiv = document.createElement("div");
+      projectDiv.classList.add("project");
 
-    const subject = projectSection.querySelector(".subject");
-    subject.textContent = project.subject;
+      const conenttDiv = document.createElement("div");
+      conenttDiv.classList.add("project-content");
 
-    const content = projectSection.querySelector(".content");
-    content.textContent = project.content;
+      const subject = document.createElement("h3"); //주제 타이틀
+      subject.classList.add("subject");
+      subject.textContent = project.subject;
 
-    const skills = projectSection.querySelector(".skills");
-    skills.textContent = "스킬: " + project.skills.join(", ");
+      const content = document.createElement("div");
+      content.classList.add("content");
+      content.textContent = project.content;
 
-    const role = projectSection.querySelector(".role");
-    role.innerHTML = "<strong>맡은 역할:</strong>";
-    project.role.forEach((roleItem) => {
-      const roleParagraph = document.createElement("p");
-      roleParagraph.textContent = roleItem;
-      role.appendChild(roleParagraph);
+      const skills = document.createElement("div");
+      skills.classList.add("skills");
+      skills.textContent = "스킬: " + project.skills.join(", ");
+
+      const role = document.createElement("div");
+      role.classList.add("role");
+      role.innerHTML = "<strong>맡은 역할:</strong>";
+      project.role.forEach((roleItem) => {
+        const roleParagraph = document.createElement("p");
+        roleParagraph.textContent = roleItem;
+        role.appendChild(roleParagraph);
+      });
+
+      const gitHubOpen = document.createElement("a");
+      gitHubOpen.classList.add("gitHubOpen");
+      gitHubOpen.href = project.git;
+
+      const youtubeOpen = document.createElement("a");
+      youtubeOpen.classList.add("youtubeOpen");
+      youtubeOpen.href = project.youtube;
+
+      conenttDiv.appendChild(subject);
+      conenttDiv.appendChild(content);
+      conenttDiv.appendChild(skills);
+      conenttDiv.appendChild(role);
+      conenttDiv.appendChild(gitHubOpen);
+      conenttDiv.appendChild(youtubeOpen);
+
+      const imgDiv = document.createElement("div");
+      imgDiv.classList.add("project-img");
+      const img = document.createElement("div");
+
+      img.classList.add("img");
+      img.style.backgroundImage = `url('../assets/images/${project.img}')`;
+      img.style.backgroundSize = "contain";
+
+      imgDiv.appendChild(img);
+      projectDiv.appendChild(conenttDiv);
+      projectDiv.appendChild(imgDiv);
+
+      projectSection.appendChild(projectDiv);
     });
-
-    const gitHubOpen = projectSection.querySelector(".gitHubOpen");
-    gitHubOpen.href = project.git;
-
-    const youtubeOpen = projectSection.querySelector(".youtubeOpen");
-    youtubeOpen.href = project.youtube;
-
-    const img = projectSection.querySelector(".project-img .img");
-    img.style.backgroundImage = `url('../assets/images/${project.img}')`;
-    img.style.backgroundSize = "contain";
   },
 };
 
